4aa3521f2d4f53c6f7c85474ede54a6d0eb10810
leila7575 <leila.louajri@gmail.com>
2024-08-14 15:08:40 +0200

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains the function to_str 
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float"""
+    return str(n)
