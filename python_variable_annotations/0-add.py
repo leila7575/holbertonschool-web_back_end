48b0e156c288308cd57a8ce9b45efb7354ece114
leila7575 <leila.louajri@gmail.com>
2024-08-14 14:10:26 +0200

    adding 0-aqdd.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,7 +4,7 @@ This module contains the function add
 """
 
 
-def add(a:float, b:float) -> float:
+def add(a: float, b: float) -> float:
     """Returns the sum of two numbers."""
     return a + b
 
