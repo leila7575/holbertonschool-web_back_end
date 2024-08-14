933171941b23cccf2ce32cfded3dcc09cd666671
leila7575 <leila.louajri@gmail.com>
2024-08-14 14:48:48 +0200

    adding 1-concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains the function concat
+"""
+
+
+def concat(str1:str, str2:str) -> str:
+    """Returns a concatenated string with str1 and str2"""
+    return str1 + str2
