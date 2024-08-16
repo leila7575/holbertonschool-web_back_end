e367016b9a0b92a74a7f92b8570538d0cdf5bc06
leila7575 <leila.louajri@gmail.com>
2024-08-16 20:21:46 +0200

    1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,6 +4,6 @@ This module contains the function concat
 """
 
 
-def concat(str1:str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     """Returns a concatenated string with str1 and str2"""
     return str1 + str2
