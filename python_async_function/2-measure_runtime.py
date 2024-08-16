05dc4492eb77dd68c227a55e8639b3bf016b630d
leila7575 <leila.louajri@gmail.com>
2024-08-16 23:34:51 +0200

    fixing pycodestyle issues

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """
-This module contains the function measure_time            
+This module contains the function measure_time
 """
 import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines').wait_n    
+
+wait_n = __import__('1-concurrent_coroutines').wait_n   
 
 
-def measure_time(n: int, max_delay: int) -> float:        
+def measure_time(n: int, max_delay: int) -> float:     
     """
     Measures the total execution time for wait_n(n, max_delay)
     returns total_time / n
--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-This module contains the function task_wait_random  
+This module contains the function task_wait_random
 """
 import asyncio
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
