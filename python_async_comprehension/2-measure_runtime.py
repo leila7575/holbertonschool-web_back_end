827921f716ed5d1c7fb3dc363881e3cd8fc87522
leila7575 <leila.louajri@gmail.com>
2024-08-19 15:52:08 +0200

    2-measure_runtime.py

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the measure_runtime coroutine.
+"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Executes async_comprehension four times in parallel
+    and measures total runtime."""
+    start = time.perf_counter()
+    coroutines = [async_comprehension(i) for i in range(4)
+    results = await asyncio.gather(*coroutines)
+    end = time.perf_counter()
+    return end - start
