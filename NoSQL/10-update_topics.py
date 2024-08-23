dff26854ec0d712365269f95a6715b5e0dd04943
leila7575 <leila.louajri@gmail.com>
2024-08-23 23:16:10 +0200

    10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" contains function update_topics."""
+from pymongo import MongoClient
+from typing import List
+
+
+def update_topics(mongo_collection, name, topics)::
+    """Changes all topics of a school document based on the name."""
+    query = {"name": name}
+    update = {"$set": {"topics": topics}}
+    mongo_collection.update_one(query, update)
