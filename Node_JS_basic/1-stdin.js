a4e524b9db82e431f6db6d37aeb64784bdd13d2f
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-18 08:37:26 +0000

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -11,5 +11,5 @@ rl.question(`Welcome to Holberton School, what is your name?\n`, (name) => {
 });
 
 rl.on(`close`, () => {
-    console.log(`This important software is now closing`);  
+    console.log(`This important software is now closing\n`);  
 });
