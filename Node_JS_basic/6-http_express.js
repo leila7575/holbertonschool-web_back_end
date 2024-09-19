f075844b6272a164aff103820c9b4c0660177750
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-19 12:44:00 +0000

    6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -4,7 +4,9 @@ const path = require('path');
 const PORT = process.env.PORT || 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+    res.setHeader("Content-Type", "text/html");
+	res.statuscode = 200;
+	res.end('Hello Holberton School!');
 });
 
 app.listen(PORT, {});
