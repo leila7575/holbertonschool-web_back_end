a35c7e3c99c833e481a2e335d199e555c5f72e27
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-19 08:04:13 +0000

    adding 4-http server file

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = 1245;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/plain");
+    res.end('Hello Holberton School!');
+
+};
+
+const app = http.createServer(requestListener);
+app.listen(port, host);
+
+module.exports = app;
