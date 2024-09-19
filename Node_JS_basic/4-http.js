5198e73ebab3a2457385fae55d4f788b3c534fcf
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-19 08:36:43 +0000

    fixing indentation on 4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,9 +4,9 @@ const host = 'localhost';
 const port = 1245;
 
 const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/plain");
-    res.end('Hello Holberton School!');
-
+	res.setHeader("Content-Type", "text/plain");
+	res.statuscode = 200;
+	res.end('Hello Holberton School!');
 };
 
 const app = http.createServer(requestListener);
