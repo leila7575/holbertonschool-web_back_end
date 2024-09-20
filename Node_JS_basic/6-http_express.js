6173fb0b877562c8c8ef7084903ad859214df0dc
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-20 16:26:22 +0000

    6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 const port = 1245;
-const host = '127.0.0.1';
+const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
 	res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
+app.listen(port, host, () => {
 	console.log(`Server listening at http://${host}:${port}`);
 });
 
