b231a5125f7e929e52b0c7f715b8b6b54a851c77
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-20 15:47:19 +0000

    4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const http = require('http');
 
-const app = express();
-const port = 1245;
 const host = '127.0.0.1';
+const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+const app = http.createServer((req, res) => {
+	res.setHeader("Content-Type", "text/plain");
+	res.statuscode = 200;
+	res.end('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://${host}:${port}`);
+app.listen(port, host, () => {
+	console.log(`Server running at http://${host}:${port}`)
 });
 
 module.exports = app;
