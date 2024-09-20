8d541d7b4130197258bfc95001efa39a48e1ca6d
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-20 17:57:08 +0000

    7-http_express.js

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,44 +1,44 @@
 const fs = require('fs').promises;
 
 function countStudents(path) {
-    return fs.readFile(path, 'utf-8')
-        .then((data) => {
-            const lines = data.trim().split('\n');
-            if (lines.length === 0) {
-                throw new Error('Cannot load the database');
-            }
+  return fs.readFile(path, 'utf-8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      if (lines.length === 0) {
+        throw new Error('Cannot load the database');
+      }
 
-            const students = [];
-            const fields = {};
+      const students = [];
+      const fields = {};
 
-            lines.slice(1).forEach((line) => {
-                const parts = line.split(',');
-                if (parts.length === 4 && parts.every((part) => part.trim().length > 0)) {
-                    const student = {
-                        firstname: parts[0],
-                        lastname: parts[1],
-                        age: parts[2],
-                        field: parts[3]
-                    };
-                    students.push(student);
+      lines.slice(1).forEach((line) => {
+        const parts = line.split(',');
+        if (parts.length === 4 && parts.every((part) => part.trim().length > 0)) {
+          const student = {
+            firstname: parts[0],
+            lastname: parts[1],
+            age: parts[2],
+            field: parts[3]
+          };
+          students.push(student);
 
-                    if (!fields[student.field]) {
-                        fields[student.field] = [];
-                    }
-                    fields[student.field].push(student.firstname);
-                }
-            });
+          if (!fields[student.field]) {
+            fields[student.field] = [];
+          }
+          fields[student.field].push(student.firstname);
+        }
+      });
 
-            console.log(`Number of students: ${students.length}`);
+      console.log(`Number of students: ${students.length}`);
 
-            for (const field in fields) {
-                const studentList = fields[field].join(', ');
-                console.log(`Number of students in ${field}: ${fields[field].length}. List: ${studentList}`);
-            }
-        })
-        .catch (() => {
-            throw new Error('Cannot load the database');
-        });
+      for (const field in fields) {
+        const studentList = fields[field].join(', ');
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${studentList}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
 }
 
 module.exports = countStudents;
--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,36 +2,36 @@ const http = require('http');
 const url = require('url');
 const countStudents = require('./3-read_file_async');
 const app = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    res.setHeader('Content-Type', 'text/plain');
+	const parsedUrl = url.parse(req.url, true);
+	res.setHeader('Content-Type', 'text/plain');
 
-    if (parsedUrl.pathname === '/') {
-        res.statusCode = 200;
-        res.end('Hello Holberton School!');
-    } else if (parsedUrl.pathname === '/students') {
-    
-        res.statusCode = 200;
-        res.write('This is the list of our students\n');
-        
-        const databaseFile = process.argv[2];
+	if (parsedUrl.pathname === '/') {
+		res.statusCode = 200;
+		res.end('Hello Holberton School!');
+	} else if (parsedUrl.pathname === '/students') {
 
-        countStudents(databaseFile)
-            .then((output) => {
-                res.write(`${output}\n`);
-                res.end();
-            })
-            .catch((err) => {
-                res.write(err.message);
-                res.end();
-            });
-    } else {
-        res.statusCode = 404;
-        res.end('Not Found');
-    }
+		res.statusCode = 200;
+		res.write('This is the list of our students\n');
+
+		const databaseFile = process.argv[2];
+
+		countStudents(databaseFile)
+			.then((output) => {
+				res.write(`${output}\n`);
+				res.end();
+			})
+			.catch((err) => {
+				res.write(err.message);
+				res.end();
+			});
+	} else {
+		res.statusCode = 404;
+		res.end('Not Found');
+	}
 });
 
 app.listen(1245, () => {
-    console.log('Server running at http://localhost:1245/');
+	console.log('Server running at http://localhost:1245/');
 });
 
 module.exports = app;
