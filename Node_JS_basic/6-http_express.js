9447f2edf9139c00b47c8b31db648c7aac26514a
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-20 07:29:10 +0000

    6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 1245;
+const port = 1245;
 
 app.get('/', (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-	res.statuscode = 200;
-	res.end('Hello Holberton School!');
+	res.send('Hello Holberton School!');
 });
 
-app.listen(PORT, {});
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app
