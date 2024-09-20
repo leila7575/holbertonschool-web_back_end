97bcc27fa95038a53533b7af593e2931dc7c64cd
leila7575 <148260175+leila7575@users.noreply.github.com>
2024-09-20 11:05:46 +0000

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
+	const name = process.stdin.read();
+	if (name) {
+		process.stdout.write(`Your name is: ${name}`);
+	}
 });
 
 process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing\n')
+	process.stdout.write('This important software is now closing\n')
 });
